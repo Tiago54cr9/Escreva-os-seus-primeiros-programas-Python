51c886a22f73ac5bb9339aeb0cb6be7447be844d
Tiago Teixeira <108500810+Tiago54cr9@users.noreply.github.com>
2022-07-07 19:09:36 +0900

    Add files via upload

---
+++ primeiros programas.py
@@ -0,0 +1,15 @@
+#primeiro = input("O seu primeiro nome:")
+#segundo = input("O seu ultimo nome:")
+#print("O seu nome é", primeiro, segundo)
+
+
+#print("Welcome to the greeter program")
+#name = input("Enter your name ")
+#print("Greetings: " + name)
+
+
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+#print(first_number + second_number)
+print(int(first_number) + int(second_number))
