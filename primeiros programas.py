ce85a300765861cf6150276f24fd29339090f7ba
Tiago Teixeira <108500810+Tiago54cr9@users.noreply.github.com>
2022-07-08 18:21:38 +0900

    Add files via upload

--- primeiros programas.py
+++ primeiros programas.py
@@ -1,15 +1,28 @@
-#primeiro = input("O seu primeiro nome:")
-#segundo = input("O seu ultimo nome:")
-#print("O seu nome é", primeiro, segundo)
+dia = input("Dia de nascimento:")
+mês = input("Mês de nacimento:")
+ano = input("Ano de nascimento:")
+print("Voçê nasceu no dia",dia,"do mês de",mês,"d5o ano",ano)
 
 
-#print("Welcome to the greeter program")
-#name = input("Enter your name ")
-#print("Greetings: " + name)
+
+print("show this in the console")
 
 
-print("calculator program")
-first_number = input("first number: ")
-second_number = input("second number: ")
-#print(first_number + second_number)
-print(int(first_number) + int(second_number))
+##Escreva os seus primeiros programas python!
+
+primeiro = input("O seu primeiro nome:")
+segundo = input("O seu ultimo nome:")
+print("O seu nome é", primeiro, segundo)
+
+
+print("Welcome to the greeter program")
+name = input("Enter your name ")
+print("Greetings: " + name)
+
+
+print("calculadora")
+primeiro_numero = input("primeiro numero: ")
+segundo_numero = input("segundo numero : ")
+#print(first_number + second_number)        junta os números por exemplo: 3+4=34
+print(int(primeiro_numero) + int(segundo_numero))
+
